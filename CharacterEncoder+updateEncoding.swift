7bfdffc9a9208bf79f5253cb1f4e91e08652c56f
CartoonChess <43363630+CartoonChess@users.noreply.github.com>
2020-05-27 22:48:20 +0900

    Finished changing to extension

--- CharacterEncoder+updateEncoding.swift
+++ CharacterEncoder+updateEncoding.swift
@@ -11,10 +11,11 @@ import Foundation
 
 extension CharacterEncoder {
     
-    static func updateCharacterEncoding(newEncoder possibleEncoder: CharacterEncoder?, oldEncoder: CharacterEncoder?, urlString: String, allowNilEncoder: Bool = false, completion: ((_ encodingDidChange: Bool) -> Void)? = nil) {
-        
-        // If the encoding ever changes, update this value, for the completion handler
-        var encodingDidChange = false
+    static func updateCharacterEncoding(newEncoder possibleEncoder: CharacterEncoder?,
+                                        oldEncoder: CharacterEncoder?,
+                                        url: URL,
+                                        allowNilEncoder: Bool = false,
+                                        completion: (encoder: CharacterEncoder?, url: URL) -> Void) {
         
         // Create mutable copy of encoder, in case using allowNilEncoder
         var temporaryEncoder = possibleEncoder
@@ -27,43 +28,34 @@ extension CharacterEncoder {
         
         // Only set encoding if not nil, as we don't want to delete any previous encoding for now
         guard let encoder = temporaryEncoder else {
-            completion?(encodingDidChange)
+            completion(oldEncoder, url)
             return
         }
         
         // And don't bother if the new encoding is the same as the old one
         if !allowNilEncoder {
-            guard encoder.encoding != newEncoder?.encoding else {
-                completion?(encodingDidChange)
+            guard encoder.encoding != oldEncoder?.encoding else {
+                completion(oldEncoder, url)
                 return
             }
         }
         
             
         // Percent-encode with the new encoding
-        let encodedUrl = encoder.encode(urlString, fullUrl: true)
+        let encodedUrlString = encoder.encode(url.absoluteString, fullUrl: true)
         
         // Check validity of newly encoded URL
         //- This should pass except when converting to UTF-8 while URL contains non-UTF characters
-        if let url = encodedUrl.encodedUrl(characterEncoder: encoder) {
-            
-            // If there is no encoder, but URL is already UTF-8 compliant, no change necessary
-            // Otherwise...
-             if !allowNilEncoder && possibleEncoder != nil {
-                // Under normal circumstances, make sure queries are encoded properly, then save them along with new encoding
-                updateEncoderAndUrl(encoder: encoder, url: url)
-                encodingDidChange = true
-            }
+        if let url = encodedUrlString.encodedUrl(characterEncoder: encoder) {
+            completion(encoder, url)
         } else {
             // If changing to UTF-8 and differently encoded characters are present, use "invalid" encoding
             //- This imitates UTF-8 while continuing to percent-encode using the custom encoder
             let invalidEncoder = CharacterEncoder(encoding: CharacterEncoder.invalidEncoding)
             
             // Check URL validity again. This should never fail
-            if let url = encodedUrl.encodedUrl(characterEncoder: invalidEncoder) {
-                updateEncoderAndUrl(encoder: invalidEncoder, url: url)
-                encodingDidChange = true
-
+            if let url = encodedUrlString.encodedUrl(characterEncoder: invalidEncoder) {
+                completion(encoder, url)
             } else if !allowNilEncoder {
                 // The URL is no longer valid
                 //- We won't do this with allowNilEncoder as that's mostly just for checking URL validity
@@ -71,15 +63,6 @@ extension CharacterEncoder {
                 fatalError("URL validity check failed even with InvalidID encoding.")
             }
         }
-        
-        // Let the caller know the encoding has changed
-        completion?(encodingDidChange)
-    }
-    
-    /// Make sure queries are encoded properly, then save them along with new encoding
-    private func updateEncoderAndUrl(encoder: CharacterEncoder, url: String) {
-        characterEncoder = encoder
-        delegate?.updateUrlDetails(baseUrl: baseUrl, queries: queries, updateView: false)
     }
     
 }
